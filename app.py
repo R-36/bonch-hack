c417c88c8e0e5d9acc80d389b24ce2749110aeda
Shaun3735 <shaun3757@gmail.com>
2021-12-11 23:42:31 +0300

    add menu to site

--- app.py
+++ app.py
@@ -66,30 +66,4 @@ def qr_cod(qr=None):
 
 @app.route('/map')
 def mapview():
-    # creating a map in the view
-    mymap = Map(
-        identifier="view-side",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[(37.4419, -122.1419)]
-    )
-    sndmap = Map(
-        identifier="sndmap",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[
-            {
-                'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-                'lat': 37.4419,
-                'lng': -122.1419,
-                'infobox': "<b>Hello World</b>"
-            },
-            {
-                'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-                'lat': 37.4300,
-                'lng': -122.1400,
-                'infobox': "<b>Hello World from other place</b>"
-            }
-        ]
-    )
-    return render_template('maps_page.html', mymap=mymap, sndmap=sndmap)
+    return render_template('maps_page.html')
