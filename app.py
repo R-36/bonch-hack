e202a27fd2d51c75e64b6f1cdc6bdf8e4eafe793
R-36 <withoutenemy@mail.ru>
2021-12-12 01:25:25 +0300

    added rest map

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template, request, flash, redirect
 
+
 from flask_qrcode import QRcode
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map
@@ -50,30 +51,5 @@ def qr_cod(qr=None):
 
 @app.route('/map')
 def mapview():
-    # creating a map in the view
-    mymap = Map(
-        identifier="view-side",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[(37.4419, -122.1419)]
-    )
-    sndmap = Map(
-        identifier="sndmap",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[
-            {
-                'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-                'lat': 37.4419,
-                'lng': -122.1419,
-                'infobox': "<b>Hello World</b>"
-            },
-            {
-                'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-                'lat': 37.4300,
-                'lng': -122.1400,
-                'infobox': "<b>Hello World from other place</b>"
-            }
-        ]
-    )
-    return render_template('maps_page.html', mymap=mymap, sndmap=sndmap)
+
+    return render_template('maps_page.html')
